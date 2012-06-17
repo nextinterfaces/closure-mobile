4c047cadc87292763ef7301f58043c6f030fb64a
Atanas Roussev <contact@nextinterfaces.com>
2012-06-17 16:46:50 -0700

    update build with next.min.js compile

--- build.sh
+++ build.sh
@@ -9,23 +9,23 @@ echo
 echo
 
 echo "---------Dependency tree ---------"
-../closure-library/closure/bin/build/closurebuilder.py \
-  --root=../closure-library/ \
-  --root=../next/ \
+./closure-library/closure/bin/build/closurebuilder.py \
+  --root=./closure-library/ \
+  --root=./web/ \
   --namespace="nx.Main"
    
 echo
 echo
   
 echo "---------Compilation starts ---------"
-../closure-library/closure/bin/build/closurebuilder.py \
-  --root=../closure-library/ \
-  --root=../next/ \
+./closure-library/closure/bin/build/closurebuilder.py \
+  --root=./closure-library/ \
+  --root=./web/ \
   --namespace="nx.Main" \
   --output_mode=compiled \
-  --compiler_jar=../compiler.jar \
+  --compiler_jar=./compiler.jar \
   --compiler_flags="--compilation_level=ADVANCED_OPTIMIZATIONS" \
-  > next.out.js
+  > next.min.js
   
   
 echo
