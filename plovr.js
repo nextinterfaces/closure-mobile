a08fe7a3f1305c3a786fd7b62703a48786276f08
Atanas Roussev <contact@nextinterfaces.com>
2012-06-17 18:01:19 -0700

    move plovr a directory up

---
+++ plovr.js
@@ -0,0 +1,25 @@
+{
+    // The id is used as a query parameter in the src of the <script> tag.
+    "id": "plovr",
+    "paths": ["web/touch", "web"],
+    "inputs": [
+        "web/touch/DragController.js",
+        "web/touch/DragEventHandler.js",
+        "web/touch/Point.js",
+        "web/model/House.js",
+        "web/model/User.js",
+        "web/view/View.js",
+        "web/view/HouseView.js",
+        "web/view/UserView.js",
+        "web/presenter/Presenter.js",
+        "web/presenter/HousePresenter.js",
+        "web/presenter/UserPresenter.js",
+        "web/Util.js",
+        "web/Event.js",
+        "web/EventBus.js",
+        "web/Ctx.js",
+        "web/Main.js"
+    ],
+    "mode": "RAW", // RAW, WHITESPACE, SIMPLE, ADVANCED
+    "level": "VERBOSE"
+}
--- web/plovr.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-<<XXX
-XXX
-
-java -jar ../plovr.jar serve plovr.js
-
